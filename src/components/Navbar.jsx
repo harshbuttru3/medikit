2c327db80c71dbfa756450cc3075248410ada59d
harsh <harsh@kali>
2024-08-28 21:35:56 +0530

    adding navigation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import './Navbar.css'
+
+function Navbar() {
+  return (
+   <>
+   <div id="nav-container">
+    <div id="home">Home</div>
+    <div id="home">Home</div>
+    <div id="home">Home</div>
+    <div id="home">Home</div>
+   </div>
+   </>
+  )
+}
+
+export default Navbar

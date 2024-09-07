5f8e3454770840588a2f5242f06adb42be51c14f
Rasnux <rasnux@localhost.localdomain>
2024-09-07 20:14:34 +0530

    added feature option

--- src/components/Integration.jsx
+++ src/components/Integration.jsx
@@ -11,13 +11,21 @@ const Integration = () => {
         <h3>Online OPD's services</h3>
         <h4 className="verify"><i className="ri-verified-badge-fill" style={{color: "#4692DD", fontSize: "25px"}}></i></h4>
       </div>
-      <div className="box" data-image="/image/heart.png">
-        <h3>Appointment for Blood donation</h3>
-        <h4 className="verify"><i className="ri-verified-badge-fill verify" style={{color: "#4692DD", fontSize: "25px"}}></i></h4>
+      <div className="box" data-image="/image/pregnent.png">
+        <h3>Pregnent ladies bed availablity</h3>
+        <h4 className="verify"><i className="ri-verified-badge-fill" style={{color: "#4692DD", fontSize: "25px"}}></i></h4>
       </div>
       <div className="box" data-image="/image/bedavail.png">
         <h3>Bed availablity check</h3>
         <h4 className="verify"><i className="ri-verified-badge-fill" style={{color: "#4692DD", fontSize: "25px"}}></i></h4>
+      </div>
+      <div className="box" data-image="/image/heart.png">
+        <h3>Appointment for Blood donation</h3>
+        <h4 className="verify"><i className="ri-verified-badge-fill verify" style={{color: "#4692DD", fontSize: "25px"}}></i></h4>
+      </div>
+      <div className="box" data-image="/image/heart.png">
+        <h3>Blood Requirement</h3>
+        <h4 className="verify"><i className="ri-verified-badge-fill verify" style={{color: "#4692DD", fontSize: "25px"}}></i></h4>
       </div>
       <div className="box" data-image="/image/ambu.jpg">
         <h3>Ambulance booking</h3>
@@ -25,10 +33,6 @@ const Integration = () => {
       </div>
       <div className="box" data-image="/image/location.png">
         <h3>Live ambulance tracking</h3>
-        <h4 className="verify"><i className="ri-verified-badge-fill" style={{color: "#4692DD", fontSize: "25px"}}></i></h4>
-      </div>
-      <div className="box" data-image="/image/pregnent.png">
-        <h3>Pregnent ladies bed availablity</h3>
         <h4 className="verify"><i className="ri-verified-badge-fill" style={{color: "#4692DD", fontSize: "25px"}}></i></h4>
       </div>
     </div>
